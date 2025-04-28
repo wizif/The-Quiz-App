97bd1da7d6be58cae8fca131eadca55b51cd6b94
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-04-28 21:44:14 +0530

    added quiz questions

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -7,6 +7,14 @@ const Quiz = () => {
         <h1>Quiz App</h1>
         <hr />
         <h2>which device is required for the internet connection?</h2>
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
       </div>
     </div>
   )
