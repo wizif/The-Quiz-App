3e58d12f59c2ad00ee288f31b1986d3706e14a7e
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-04-28 21:38:44 +0530

    the first commit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Quiz.css"
+const Quiz = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Quiz App</h1>
+        <hr />
+        <h2></h2>
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
